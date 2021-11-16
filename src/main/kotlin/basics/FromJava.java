969a0fa05c35c2e297dcdff6bf7b3e574a6245f4
Sindhuja-Datla <sindhujadatla20@gmail.com>
2021-11-16 10:46:21 +0530

    Kotlin Basics

---
+++ src/main/kotlin/basics/FromJava.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class FromJava {
+
+
+        public static void main(String[] args) {
+            Student sindhu = new Student("Sindhuja Datla",123,876);
+            System.out.println(sindhu.getName());
+
+    }
+}
